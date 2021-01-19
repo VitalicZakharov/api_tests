00bed5c4b2b4819ff4e9f08d270f4831fb16941e
VitalicZakharov <vitalic.zakharov@brightpattern.com>
2021-01-19 17:48:26 +0300

    tests grouping by response codes

--- tests/test_delete_all_records.py
+++ tests/test_delete_all_records.py
@@ -1,6 +1,10 @@
 import pytest
 import allure
 
+#=======================================================================================================================
+#==================================================== Code 200 =========================================================
+#=======================================================================================================================
+
 @pytest.mark.usefixtures("delete_all_records_post_request_with_a_valid_list_assigned_to_multiple_campaigns")
 class Test_post_request_with_a_valid_list_assigned_to_multiple_campaigns():
     @allure.epic("test_delete_all_records")
@@ -64,6 +68,34 @@ class Test_post_request_with_a_valid_list_assigned_to_multiple_campaigns_empty_b
             delete_all_records_post_request_with_a_valid_list_assigned_to_multiple_campaigns_empty_body_and_list_without_records.text)
 
 
+@pytest.mark.usefixtures("delete_all_records_post_request_with_csv_list_format")
+class Test_post_request_with_csv_list_format():
+    @allure.epic("test_delete_all_records")
+    @allure.feature("answer code 200")
+    @allure.step('test_check_status_code_post_request_with_csv_list_format')
+    def test_check_status_code_post_request_with_csv_list_format(self, delete_all_records_post_request_with_csv_list_format):
+        print("request_result_status_code : ", delete_all_records_post_request_with_csv_list_format.status_code)
+        assert "200" in str(
+            delete_all_records_post_request_with_csv_list_format.status_code), "Answer status not 200 ; actual status code : " + str(
+            delete_all_records_post_request_with_csv_list_format.status_code)
+
+    @allure.epic("test_delete_all_records")
+    @allure.feature("answer code 200")
+    @allure.step('test_check_answer_text_post_request_with_csv_list_format')
+    def test_check_answer_text_post_request_with_csv_list_format(self, delete_all_records_post_request_with_csv_list_format):
+        print("request_result_text : ", delete_all_records_post_request_with_csv_list_format.text)
+        assert len(str(
+            delete_all_records_post_request_with_csv_list_format.text)) == 0, "Answer text not empty ; actual message : " + str(
+            delete_all_records_post_request_with_csv_list_format.text)
+
+#=======================================================================================================================
+#=======================================================================================================================
+#=======================================================================================================================
+
+#=======================================================================================================================
+#==================================================== Code 404 =========================================================
+#=======================================================================================================================
+
 @pytest.mark.usefixtures("delete_all_records_post_request_with_an_invalid_list")
 class Test_post_request_with_an_invalid_list():
     @allure.epic("test_delete_all_records")
@@ -85,6 +117,35 @@ class Test_post_request_with_an_invalid_list():
             delete_all_records_post_request_with_an_invalid_list.text), "Answer text not " + status + " ; actual message : " + str(
             delete_all_records_post_request_with_an_invalid_list.text)
 
+
+@pytest.mark.usefixtures("delete_all_records_post_request_with_invalid_url")
+class Test_post_request_with_invalid_url():
+    @allure.epic("test_delete_all_records")
+    @allure.feature("answer code 404")
+    @allure.step('test_check_status_code_post_request_with_invalid_url')
+    def test_check_status_code_post_request_with_invalid_url(self, delete_all_records_post_request_with_invalid_url):
+        print("request_result_status_code : ", delete_all_records_post_request_with_invalid_url.status_code)
+        assert "404" in str(
+            delete_all_records_post_request_with_invalid_url.status_code), "Answer status not 404 ; actual status code : " + str(
+            delete_all_records_post_request_with_invalid_url.status_code)
+
+    @allure.epic("test_delete_all_records")
+    @allure.feature("answer code 404")
+    @allure.step('test_check_answer_text_post_request_with_invalid_url')
+    def test_check_answer_text_post_request_with_invalid_url(self, delete_all_records_post_request_with_invalid_url):
+        print("request_result_text : ", delete_all_records_post_request_with_invalid_url.text)
+        status = "HTTP 404 Not Found"
+        assert status in str(
+            delete_all_records_post_request_with_invalid_url.text), "Answer text not " + status + " ; actual message : " + str(
+            delete_all_records_post_request_with_invalid_url.text)
+
+#=======================================================================================================================
+#=======================================================================================================================
+#=======================================================================================================================
+
+#=======================================================================================================================
+#==================================================== Code 401 =========================================================
+#=======================================================================================================================
 
 @pytest.mark.usefixtures("delete_all_records_post_request_with_do_not_authorize_session")
 class Test_post_request_with_do_not_authorize_session():
@@ -109,6 +170,13 @@ class Test_post_request_with_do_not_authorize_session():
             delete_all_records_post_request_with_do_not_authorize_session.text), "Answer text not " + status + " ; actual message : " + str(
             delete_all_records_post_request_with_do_not_authorize_session.text)
 
+#=======================================================================================================================
+#=======================================================================================================================
+#=======================================================================================================================
+
+#=======================================================================================================================
+#==================================================== Code 403 =========================================================
+#=======================================================================================================================
 
 #@allure.issue("https://trac.brightpattern.com/ticket/24588")
 @pytest.mark.usefixtures("delete_all_records_post_request_with_authorize_session_for_user_without_permission")
@@ -134,28 +202,13 @@ class Test_post_request_with_authorize_session_for_user_without_permission():
             delete_all_records_post_request_with_authorize_session_for_user_without_permission.text), "Answer text not " + status + " ; actual message : " + str(
             delete_all_records_post_request_with_authorize_session_for_user_without_permission.text)
 
+#=======================================================================================================================
+#=======================================================================================================================
+#=======================================================================================================================
 
-@pytest.mark.usefixtures("delete_all_records_post_request_with_invalid_url")
-class Test_post_request_with_invalid_url():
-    @allure.epic("test_delete_all_records")
-    @allure.feature("answer code 404")
-    @allure.step('test_check_status_code_post_request_with_invalid_url')
-    def test_check_status_code_post_request_with_invalid_url(self, delete_all_records_post_request_with_invalid_url):
-        print("request_result_status_code : ", delete_all_records_post_request_with_invalid_url.status_code)
-        assert "404" in str(
-            delete_all_records_post_request_with_invalid_url.status_code), "Answer status not 404 ; actual status code : " + str(
-            delete_all_records_post_request_with_invalid_url.status_code)
-
-    @allure.epic("test_delete_all_records")
-    @allure.feature("answer code 404")
-    @allure.step('test_check_answer_text_post_request_with_invalid_url')
-    def test_check_answer_text_post_request_with_invalid_url(self, delete_all_records_post_request_with_invalid_url):
-        print("request_result_text : ", delete_all_records_post_request_with_invalid_url.text)
-        status = "HTTP 404 Not Found"
-        assert status in str(
-            delete_all_records_post_request_with_invalid_url.text), "Answer text not " + status + " ; actual message : " + str(
-            delete_all_records_post_request_with_invalid_url.text)
-
+#=======================================================================================================================
+#==================================================== Code 405 =========================================================
+#=======================================================================================================================
 
 #@allure.issue("https://trac.brightpattern.com/ticket/24576")
 @pytest.mark.usefixtures("delete_all_records_get_request_with_correct_body")
@@ -225,6 +278,13 @@ class Test_delete_request_with_correct_body():
             delete_all_records_delete_request_with_correct_body.text), "Answer text not " + status + " ; actual message : " + str(
             delete_all_records_delete_request_with_correct_body.text)
 
+#=======================================================================================================================
+#=======================================================================================================================
+#=======================================================================================================================
+
+#=======================================================================================================================
+#==================================================== Code 400 =========================================================
+#=======================================================================================================================
 
 @allure.issue("https://trac.brightpattern.com/ticket/24577")
 @pytest.mark.usefixtures("delete_all_records_post_request_with_incorrect_body_format_typization")
@@ -250,24 +310,6 @@ class Test_post_request_with_incorrect_body_format_typization():
             delete_all_records_post_request_with_incorrect_body_format_typization.text), "Answer text not " + status + " ; actual message : " + str(
             delete_all_records_post_request_with_incorrect_body_format_typization.text)
 
-
-@pytest.mark.usefixtures("delete_all_records_post_request_with_csv_list_format")
-class Test_post_request_with_csv_list_format():
-    @allure.epic("test_delete_all_records")
-    @allure.feature("answer code 200")
-    @allure.step('test_check_status_code_post_request_with_csv_list_format')
-    def test_check_status_code_post_request_with_csv_list_format(self, delete_all_records_post_request_with_csv_list_format):
-        print("request_result_status_code : ", delete_all_records_post_request_with_csv_list_format.status_code)
-        assert "200" in str(
-            delete_all_records_post_request_with_csv_list_format.status_code), "Answer status not 200 ; actual status code : " + str(
-            delete_all_records_post_request_with_csv_list_format.status_code)
-
-    @allure.epic("test_delete_all_records")
-    @allure.feature("answer code 200")
-    @allure.step('test_check_answer_text_post_request_with_csv_list_format')
-    def test_check_answer_text_post_request_with_csv_list_format(self, delete_all_records_post_request_with_csv_list_format):
-        print("request_result_text : ", delete_all_records_post_request_with_csv_list_format.text)
-        assert len(str(
-            delete_all_records_post_request_with_csv_list_format.text)) == 0, "Answer text not empty ; actual message : " + str(
-            delete_all_records_post_request_with_csv_list_format.text)
-
+#=======================================================================================================================
+#=======================================================================================================================
+#=======================================================================================================================
